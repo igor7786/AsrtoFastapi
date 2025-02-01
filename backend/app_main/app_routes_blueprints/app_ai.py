52251b2dcfed77332e2b89f75d06b20d50d5e78f
igor7786 <grimuta60@gmail.com>
2025-02-01 08:46:52 +0000

    frontend, backend, error handaling updated

--- backend/app_main/app_routes_blueprints/app_ai.py
+++ backend/app_main/app_routes_blueprints/app_ai.py
@@ -1,11 +1,10 @@
 from app_main.app_imports import StreamingResponse, BaseModel, APIRouter, Annotated, Optional, List, File, UploadFile, \
-	Form, io, Image
-from app_main.app_dependancies_helpers_global_vars.helpers import stream_text_gemma, stream_text_gemmini, _stream_error
+	Form, io, Image, JSONResponse
+from app_main.app_dependancies_helpers_global_vars.helpers import stream_text_gemma, stream_text_gemmini
 
 
 class Query(BaseModel):
 	prompt: str
-	attachments: Optional[List[str]] = None
 
 
 router = APIRouter(prefix="/v1/AI", tags=["AI-Models"])
@@ -15,39 +14,29 @@ router = APIRouter(prefix="/v1/AI", tags=["AI-Models"])
 async def generate_text(query: Query):
 	q = query.prompt
 	if not q.strip():
-		return {"error": "Prompt cannot be empty"}
-	if query.attachments:
-		attachments = query.attachments
-		return StreamingResponse(
-			stream_text_gemmini(q, attachments),
-			media_type="text/event-stream",  # Ensure no caching of the response
-		)
+		return JSONResponse(content={"error": "Prompt cannot be empty"}, status_code=422)
 	return StreamingResponse(
 		stream_text_gemma(q),
 		media_type="text/event-stream",  # Ensure no caching of the response
+		status_code=201
 	)
-
-
-# Define the Pydantic model for the request body
-class AIRequest(BaseModel):
-	prompt: Annotated[str, Form()]
-	files: Annotated[Optional[List[UploadFile]], Form()] = None  # Files are optional
 
 
 # Define the endpoint to receive the request
 @router.post("/generate-gemmini")
-async def receive_data(prompt: Annotated[str, Form()], files: Annotated[list[UploadFile], File()] = None):
+async def receive_data(prompt: Annotated[str, Form()], files: Annotated[Optional[list[UploadFile]], File()] = None):
 	try:
 		if not prompt.strip():
-			return StreamingResponse(_stream_error(), media_type="text/event-stream")
+			return JSONResponse(content={"error": "Prompt cannot be empty"}, status_code=422)
 		if not files:
-			return StreamingResponse(stream_text_gemmini(prompt), media_type="text/event-stream")
+			return StreamingResponse(stream_text_gemmini(prompt), media_type="text/event-stream", status_code=201)
 		for f in files:
 			# Read file contents
 			contents = await f.read()
 			# Convert to Image using PIL
 			image = Image.open(io.BytesIO(contents))
-			return StreamingResponse(stream_text_gemmini(prompt, image), media_type="text/event-stream")
-
+			return StreamingResponse(
+				stream_text_gemmini(prompt, image), media_type="text/event-stream", status_code=201
+			)
 	except Exception:
-		return StreamingResponse(_stream_error(), media_type="text/event-stream")
+		return JSONResponse(content={"error": "Something went wrong"}, status_code=400)
--- frontend/src/pages/api/chat.ts
+++ frontend/src/pages/api/chat.ts
@@ -1,12 +1,11 @@
 import type { APIRoute } from 'astro';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 function errorHandler(error: string) {
-  const err =
-    '0:"Please load a single file, servers are limited!!"\n' +
-    'e:{"finishReason":"stop","usage":{"promptTokens":null,"completionTokens":null},"isContinued":false}\n';
-  console.log(err);
+  const err = '0:"' + error + '"\n';
+  ('e:{"finishReason":"stop","usage":{"promptTokens":null,"completionTokens":null},"isContinued":false}\n');
   return new Response(err, {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
@@ -17,14 +16,7 @@ export const POST: APIRoute = async ({ request }) => {
   const { messages } = await request.json();
   const fastAPIUrl = `${process.env.API_AI_ENDPOINT}`;
   if (!messages) {
-    const noMessage =
-      '0:"No message provided!!"\n' +
-      'e:{"finishReason":"stop","usage":{"promptTokens":null,"completionTokens":null},"isContinued":false}\n';
-    return new Response(noMessage, {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
-    // errorHandler('No message provided!!');
+    return errorHandler('No message provided!!');
   }
 
   const lastMessage = messages[messages.length - 1];
@@ -35,13 +27,6 @@ export const POST: APIRoute = async ({ request }) => {
   formData.append('prompt', prompt);
 
   if (lastMessage?.experimental_attachments && lastMessage.experimental_attachments.length > 1) {
-    // const noMessage =
-    //   '0:"Please load a single file, servers are limited!!"\n' +
-    //   'e:{"finishReason":"stop","usage":{"promptTokens":null,"completionTokens":null},"isContinued":false}\n';
-    // return new Response(noMessage, {
-    //   status: 200,
-    //   headers: { 'Content-Type': 'application/json' },
-    // });
     return errorHandler('Please load a single file, servers are limited!!');
   }
   lastMessage.experimental_attachments?.forEach((att: any, index: number) => {
@@ -69,9 +54,11 @@ export const POST: APIRoute = async ({ request }) => {
       method: 'POST',
       body: formData, // Send as multipart/form-data
     });
-
+    if (!response.ok) {
+      return errorHandler('Could not fetch response from Server try again Later!!');
+    }
     return new Response(response.body, {
-      status: 200,
+      status: 201,
       headers: {
         'Content-Type': 'text/plain; charset=utf-8',
         'X-Vercel-AI-Data-Stream': 'v1',
@@ -80,17 +67,6 @@ export const POST: APIRoute = async ({ request }) => {
       },
     });
   } catch (error) {
-    const err =
-      '0:"Could not fetch response from Server try again Later!!"\n' +
-      'e:{"finishReason":"stop","usage":{"promptTokens":null,"completionTokens":null},"isContinued":false}\n';
-    return new Response(err, {
-      status: 200,
-      headers: {
-        'Content-Type': 'text/plain; charset=utf-8',
-        'X-Vercel-AI-Data-Stream': 'v1',
-        'Transfer-Encoding': 'chunked',
-        'Cache-Control': 'no-cache',
-      },
-    });
+    return errorHandler('Could not fetch response from Server try again Later!!!');
   }
 };
