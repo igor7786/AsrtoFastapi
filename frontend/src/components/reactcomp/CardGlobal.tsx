da19cbce2e4a56559a95b423089324b903f25b96
igor7786 <grimuta60@gmail.com>
2025-02-08 09:10:50 +0000

    frontend-> astro dinamical endpoin updated-> added delay,transtack,HTMX

--- frontend/src/components/reactcomp/CardGlobal.tsx
+++ frontend/src/components/reactcomp/CardGlobal.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Plus, Minus } from 'lucide-react';
 import { Button } from '@/components/reactcomp/ui/button';
+import { Spinner } from '@/components/reactcomp/ui/spinner';
 import {
   Card,
   CardContent,
@@ -9,34 +10,20 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/reactcomp/ui/card';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useStore } from '@nanostores/react';
 import { count, err } from '@/components/reactcomp/reactlib/utils.ts';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { queryClient } from '@/components/reactcomp/reactlib/utils.ts';
 
 export default function CardGlobalState() {
   useEffect(() => {
     // @ts-ignore
+    // ! initialize htmx to work in react
     window.htmx.process(document.body);
   }, []);
 
-  const fetchNewState = async () => {
-    try {
-      const response = await fetch('partials/1', { method: 'POST' });
-      // Ensure the response is not empty before parsing JSON
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      const text = await response.text();
-      if (!text) {
-        throw new Error('Empty response from server');
-      }
-      const data = JSON.parse(text);
-      count.set(data.count);
-    } catch (error) {
-      err.set('Somthing went wrong!'); // This will set the error message in the err store
-    }
-  };
-
+  // ! Nano stores
   const counter = useStore(count);
   const errorMessage = useStore(err);
   const increment = () => {
@@ -48,11 +35,35 @@ export default function CardGlobalState() {
       err.set("Value can't be below zero!");
     } else {
       count.set(counter - 1);
+      err.set('');
     }
   };
   const add = () => increment();
   const sub = () => decrement();
+  // ! Tanstack Query
+  const getData = async () => {
+    const response = await fetch('/partials/1', { method: 'POST' });
 
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return response.json();
+  };
+  const mutation = useMutation(
+    {
+      mutationKey: ['reset'],
+      mutationFn: getData,
+      onSuccess: (data) => {
+        count.set(data.count); // Update counter with the response
+        err.set('');
+      },
+      onError: () => {
+        err.set('Something went wrong!');
+      },
+    },
+    queryClient
+  );
   return (
     <Card className="w-[340px]">
       <CardHeader>
@@ -65,7 +76,7 @@ export default function CardGlobalState() {
             <div id="counter">{counter}</div>
           </div>
         ) : (
-          <div className="text-2xl font-bold text-white">
+          <div className="text-2xl font-bold text-black dark:text-white">
             <div id="counter">{counter}</div>
           </div>
         )}
@@ -84,10 +95,17 @@ export default function CardGlobalState() {
         </div>
         <Button
           className="mt-4 text-center font-bold text-white"
-          onClick={fetchNewState}
-          disabled={!!errorMessage || counter === 0}
+          onClick={() => mutation.mutate()}
+          disabled={mutation.isPending || counter === 0}
         >
-          Reset
+          {mutation.isPending ? (
+            <div className="flex items-center justify-between">
+              <Spinner className="mr-2 text-white"></Spinner>
+              <span className="text-cyan-white">Resting</span>
+            </div>
+          ) : (
+            'Reset-Fetch'
+          )}
         </Button>
       </CardFooter>
     </Card>
--- frontend/src/components/reactcomp/reactlib/utils.ts
+++ frontend/src/components/reactcomp/reactlib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { atom } from 'nanostores';
+import { QueryClient } from '@tanstack/react-query';
+export const queryClient = new QueryClient();
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
