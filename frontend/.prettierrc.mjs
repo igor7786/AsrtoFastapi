78cbe83c5d23257903e52ff8c125662d827f77ad
Igor7786 <grimuta60@gmail.com>
2025-09-06 15:24:15 +0000

    vscode-transfer-3

--- frontend/.prettierrc.mjs
+++ frontend/.prettierrc.mjs
@@ -1,10 +1,10 @@
-/** @type {import('prettier').Config} */
+/** @type {import("prettier").Config} */
 export default {
   plugins: [
     'prettier-plugin-astro',
-    'prettier-plugin-tailwindcss',
     'prettier-plugin-classnames',
     'prettier-plugin-merge',
+    'prettier-plugin-tailwindcss',
   ],
   printWidth: 105, // Use this to limit line length globally, including classes
   proseWrap: 'always',
@@ -16,7 +16,6 @@ export default {
   bracketSpacing: true,
   arrowParens: 'always',
   // tailwindConfig: './tailwind.config.mjs',
-  tailwindStylesheet: './src/styles/global.css',
   overrides: [
     {
       files: '*.astro',
