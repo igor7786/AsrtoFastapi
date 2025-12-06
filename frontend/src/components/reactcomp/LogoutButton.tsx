1ada0333250cb6c5c6b6ac33de7a166918605bc7
Igor7786 <grimuta60@gmail.com>
2025-12-06 13:17:28 +0000

    feat(auth): implement logout functionality with toast notifications

--- frontend/src/components/reactcomp/LogoutButton.tsx
+++ frontend/src/components/reactcomp/LogoutButton.tsx
@@ -2,12 +2,69 @@ import { Spinner } from '@/components/reactcomp/spinner';
 import { getQueryClient } from '@/lib/tan-stack/tanstack-query';
 import { toast } from 'sonner';
 import { Check, X } from 'lucide-react';
+import { useMutation } from '@tanstack/react-query';
+import { client } from '@hono-adapt/orpc/client';
 
 export default function LogoutButton() {
   const queryClient = getQueryClient();
+  const idToast = 'login-toast';
+
+  const mutation = useMutation(
+    {
+      mutationFn: () => client.auth.logout(),
+
+      onMutate: async () => {
+        toast(
+          <div className="flex items-center gap-2">
+            <Spinner className="text-orange-500" />
+            <span>Logging out...</span>
+          </div>,
+          {
+            id: idToast,
+            duration: Infinity,
+          }
+        );
+        await new Promise((resolve) => setTimeout(resolve, 500));
+      },
+
+      onError: async (error) => {
+        toast(
+          <div className="flex items-center gap-2">
+            <X className="text-red-500" />
+            <span>{error.message ?? 'Failed to logout from account.'}</span>
+          </div>,
+          {
+            id: idToast,
+            duration: 1000,
+          }
+        );
+      },
+
+      onSuccess: async (data) => {
+        toast(
+          <div className="flex items-center gap-2">
+            <Check className="text-green-500" />
+            <span>{data.message}</span>
+          </div>,
+          {
+            id: idToast,
+            duration: 1000,
+          }
+        );
+        const timer = setTimeout(() => {
+          window.location.reload();
+        }, 500);
+        return () => {
+          clearTimeout(timer);
+        };
+      },
+    },
+    queryClient
+  );
 
   const handleLogout = () => {
     queryClient.clear();
+    mutation.mutate();
   };
 
   return (
